0542adb0c54913bcb4ec150959e14d5053f113ed
Nick Kinlen <nick_kinlen@hotmail.com>
2017-11-13 17:30:15 -0500

    Added waypoint.js to trigger the animations on scroll, fully working

--- js/app.js
+++ js/app.js
@@ -1,39 +1,33 @@
 
-  var bar = new ProgressBar.Circle(container, {
-    color: '#0AA1DA',
-    // This has to be the same size as the maximum width to
-    // prevent clipping
-    strokeWidth: 6,
-    trailWidth: 1,
-    easing: 'easeInOut',
-    duration: 1400,
-    text: {
-      autoStyleContainer: false
-    },
-    from: { color: '#0AA1DA', width: 1 },
-    to: { color: '#0AA1DA', width: 6 },
-    // Set default step function for all animate calls
-    step: function(state, circle) {
-      circle.path.setAttribute('stroke', state.color);
-      circle.path.setAttribute('stroke-width', state.width);
-      circle.setText('Design');
 
-  /*    var value = Math.round(circle.value() * 100);
-      if (value === 0) {
-        circle.setText('');
-      } else {
-        circle.setText(value);
+    var bar = new ProgressBar.Circle(container, {
+      color: '#0AA1DA',
+      // This has to be the same size as the maximum width to
+      // prevent clipping
+      strokeWidth: 6,
+      trailWidth: 1,
+      easing: 'easeInOut',
+      duration: 1400,
+      text: {
+        autoStyleContainer: false
+      },
+      from: { color: '#0AA1DA', width: 1 },
+      to: { color: '#0AA1DA', width: 6 },
+      // Set default step function for all animate calls
+      step: function(state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+        circle.path.setAttribute('stroke-width', state.width);
+        circle.setText('Design');
+
       }
-  */
+    });
 
-    }
-  });
-  bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
-  bar.text.style.fontSize = '1rem';
-  bar.text.style.fontWeight = 'bolder';
-  bar.text.style.color= 'black';
+    bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+    bar.text.style.fontSize = '1rem';
+    bar.text.style.fontWeight = 'bolder';
+    bar.text.style.color= 'black';
 
-  bar.animate(1.0);  // Number from 0.0 to 1.0
+    //bar.animate(1.0);
 
 
   var bar2 = new ProgressBar.Circle(container2, {
@@ -63,7 +57,6 @@
   bar2.text.style.fontWeight = 'bolder';
   bar2.text.style.color= 'black';
 
-  bar2.animate(0.9);
 
 
   var bar3 = new ProgressBar.Circle(container3, {
@@ -94,8 +87,6 @@
   bar3.text.style.color= 'black';
   bar3.text.style.textAlign = 'center';
 
-  bar3.animate(0.8);
-
 
   var bar4 = new ProgressBar.Circle(container4, {
     color: '#0AA1DA',
@@ -124,8 +115,6 @@
   bar4.text.style.fontWeight = 'bolder';
   bar4.text.style.color= 'black';
 
-  bar4.animate(0.7);
-
 
   var bar5 = new ProgressBar.Circle(container5, {
     color: '#0AA1DA',
@@ -153,17 +142,26 @@
   bar5.text.style.fontWeight = 'bolder';
   bar5.text.style.color= 'black';
 
-  bar5.animate(0.6);
 
-
+  var waypoint = new Waypoint({
+    element: document.querySelector(".circle-container"),
+    handler: function(direction) {
+      bar.animate(1.0);
+      bar2.animate(0.9);
+      bar3.animate(0.7);
+      bar4.animate(0.6);
+      bar5.animate(0.5);
+    },
+      offset: '75%'
+  });
 
 
 
   //********************************************************************************
 
 
-  var bar = new ProgressBar.Line(line, {
-    strokeWidth: 4,
+  var a = new ProgressBar.Line(line, {
+    strokeWidth: 5,
     easing: 'easeInOut',
     duration: 1400,
     color: '#0AA1DA',
@@ -177,8 +175,9 @@
     }
   });
 
-  var bar2 = new ProgressBar.Line(line, {
-  strokeWidth: 4,
+
+  var b = new ProgressBar.Line(line, {
+  strokeWidth: 5,
   easing: 'easeInOut',
   duration: 1400,
   color: '#0AA1DA',
@@ -192,8 +191,8 @@
   }
 });
 
-  var bar3 = new ProgressBar.Line(line, {
-  strokeWidth: 4,
+  var c = new ProgressBar.Line(line, {
+  strokeWidth: 5,
   easing: 'easeInOut',
   duration: 1400,
   color: '#0AA1DA',
@@ -207,8 +206,8 @@
   }
 });
 
-  var bar4 = new ProgressBar.Line(line, {
-  strokeWidth: 4,
+  var d = new ProgressBar.Line(line, {
+  strokeWidth: 5,
   easing: 'easeInOut',
   duration: 1400,
   color: '#0AA1DA',
@@ -222,8 +221,8 @@
   }
 });
 
-  var bar5 = new ProgressBar.Line(line, {
-  strokeWidth: 4,
+  var e = new ProgressBar.Line(line, {
+  strokeWidth: 5,
   easing: 'easeInOut',
   duration: 1400,
   color: '#0AA1DA',
@@ -237,8 +236,8 @@
   }
 });
 
-  var bar6 = new ProgressBar.Line(line, {
-  strokeWidth: 4,
+  var f = new ProgressBar.Line(line, {
+  strokeWidth: 5,
   easing: 'easeInOut',
   duration: 1400,
   color: '#0AA1DA',
@@ -252,10 +251,15 @@
   }
 });
 
-
-bar.animate(1.0);
-bar2.animate(0.9);
-bar3.animate(0.8);
-bar4.animate(0.7);
-bar5.animate(0.6);
-bar6.animate(0.5);
+var waypoint2 = new Waypoint({
+  element: document.getElementById("software"),
+  handler: function(direction) {
+    a.animate(1.0);
+    b.animate(0.9);
+    c.animate(0.8);
+    d.animate(0.7);
+    e.animate(0.6);
+    f.animate(0.5);
+  },
+    offset: '75%'
+});
